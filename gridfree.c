28dd6cab7094785292fe58aab0b6a04bbdea50c8
reem-3 <113852082+reem-3@users.noreply.github.com>
2022-12-12 10:01:18 +0300

    Update gridfree.c

--- gridfree.c
+++ gridfree.c
@@ -1,1 +1,17 @@
-gridfree.c
+#include "sshell.h"
+/**
+ * gridfree - free a matrix
+ * @grid: it is the matrix
+ * @height: height of the matrix
+ * Return: nothing
+ */
+void gridfree(char **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
