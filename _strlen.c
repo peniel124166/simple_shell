c0dff05e73f39fa107b5f1377005716d49570730
reem-3 <113852082+reem-3@users.noreply.github.com>
2022-12-12 09:49:34 +0300

    Update _strlen.c

--- _strlen.c
+++ _strlen.c
@@ -1,1 +1,18 @@
-_strlen.c
+#include "sshell.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
