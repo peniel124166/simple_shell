4c6c2939d4c2320e63873c797d434ce42052e4b0
reem-3 <113852082+reem-3@users.noreply.github.com>
2022-12-12 09:47:14 +0300

    Update _signal.c

--- _signal.c
+++ _signal.c
@@ -1,1 +1,11 @@
-_signal.c
+#include "sshell.h"
+
+/**
+ * _signal - handle SIGINT signal
+ * @s: signal to catch
+ */
+void _signal(int s)
+{
+	(void)s;
+	write(STDOUT_FILENO, "\n#cisfun$ ", 10);
+}
