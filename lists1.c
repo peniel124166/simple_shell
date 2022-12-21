35005bdad54b7baff4d45203de6c158bec2ff133
reem-3 <113852082+reem-3@users.noreply.github.com>
2022-12-21 11:52:52 -0800

    Update lists1.c

--- lists1.c
+++ lists1.c
@@ -1,101 +1,122 @@
-#include "sshell.h"
-/**
- * _ishelp - if args[0] is help builtin
- * @p: input pointer
- * @loop: counter of loops
- * @v: arguments in input
- * @m: copy of environment variables
- * Return: 0 if help exist or -1 if not.
- */
-int _ishelp(char **p, int loop, char *v[], char **m)
-{
-	char str[] = "help";
-	int i = 0, cont = 0, salida = -1;
+#include "shell.h"
 
-	while (p[0][i])
+/**
+ * list_len - determines length of linked list
+ * @h: pointer to first node
+ *
+ * Return: size of list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
 	{
-		if (i < 4)
-		{
-			if (p[0][i] == str[i])
-				cont++;
-		}
+		h = h->next;
 		i++;
 	}
-	if (i == 4)
-		cont++;
-
-	if (cont == 5)
-	{
-		_help(p, loop, v, m);
-		salida = 0;
-	}
-	return (salida);
+	return (i);
 }
+
 /**
- * _help_builtin - help of builtin
- * @p: input pointer
- * @loop: counter of loops
- * @v: arguments in input
- * @m: copy of environment variables
- * Return: nothing.
+ * list_to_strings - returns an array of strings of the list->str
+ * @head: pointer to first node
+ *
+ * Return: array of strings
  */
-void _help_builtin(char **p, int loop, char *v[], char **m)
+char **list_to_strings(list_t *head)
 {
-	char str1[3] = "cd", str2[5] = "exit", str3[5] = "help";
-	int i = 0, j = 0, cont = 0, cont2 = 0, k = 1;
+	list_t *node = head;
+	size_t i = list_len(head), j;
+	char **strs;
+	char *str;
 
-	while (p[k] != NULL)
+	if (!head || !i)
+		return (NULL);
+	strs = malloc(sizeof(char *) * (i + 1));
+	if (!strs)
+		return (NULL);
+	for (i = 0; node; node = node->next, i++)
 	{
-		j = 0;
-		cont = 0, cont2 = 0;
-		while (p[k][j] != '\0')
-			j++;
-		if (j == 2)
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
 		{
-			for (i = 0; i < 2; i++)
-				if (p[k][i] == str1[i])
-					cont++;
-			if (cont == 2)
-				read_cdhelp(m);
-			else
-				_put_err(p, loop, 1, v);
+			for (j = 0; j < i; j++)
+				free(strs[j]);
+			free(strs);
+			return (NULL);
 		}
-		else if (j == 4)
-		{
-			for (i = 0; i < 4; i++)
-				if (p[k][i] == str2[i])
-					cont++;
-			if (cont == 4)
-				read_exithelp(m);
-			for (i = 0; i < 4; i++)
-				if (p[k][i] == str3[i])
-					cont2++;
-			if (cont2 == 4)
-				read_helphelp(m);
-			else if (cont != 4 && cont2 != 4)
-				_put_err(p, loop, 1, v);
-		}
-		else
-			_put_err(p, loop, 1, v);
-		k++;
+
+		str = _strcpy(str, node->str);
+		strs[i] = str;
 	}
+	strs[i] = NULL;
+	return (strs);
 }
+
+
 /**
-* _help- writes the error
-* @p: input pointer
-* @loop: counter of loops
-* @v: arguments in input
-* @m: copy of environment variables
-* Return: nothing
-*/
-void _help(char **p, int loop, char *v[], char **m)
+ * print_list - prints all elements of a list_t linked list
+ * @h: pointer to first node
+ *
+ * Return: size of list
+ */
+size_t print_list(const list_t *h)
 {
-	if (p[0] != NULL && p[1] == NULL)
+	size_t i = 0;
+
+	while (h)
 	{
-		read_help(m);
+		_puts(convert_number(h->num, 10, 0));
+		_putchar(':');
+		_putchar(' ');
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		i++;
 	}
-	else
+	return (i);
+}
+
+/**
+ * node_starts_with - returns node whose string starts with prefix
+ * @node: pointer to list head
+ * @prefix: string to match
+ * @c: the next character after prefix to match
+ *
+ * Return: match node or null
+ */
+list_t *node_starts_with(list_t *node, char *prefix, char c)
+{
+	char *p = NULL;
+
+	while (node)
 	{
-		_help_builtin(p, loop, v, m);
+		p = starts_with(node->str, prefix);
+		if (p && ((c == -1) || (*p == c)))
+			return (node);
+		node = node->next;
 	}
+	return (NULL);
+}
+
+/**
+ * get_node_index - gets the index of a node
+ * @head: pointer to list head
+ * @node: pointer to the node
+ *
+ * Return: index of node or -1
+ */
+ssize_t get_node_index(list_t *head, list_t *node)
+{
+	size_t i = 0;
+
+	while (head)
+	{
+		if (head == node)
+			return (i);
+		head = head->next;
+		i++;
+	}
+	return (-1);
 }
