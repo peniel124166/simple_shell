d6f65b50f4b430df9c965ec24856590a4ad4bc2a
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2022-12-21 09:00:59 -0800

    Update getenv.c

--- getenv.c
+++ getenv.c
@@ -1,44 +1,93 @@
-#include "sshell.h"
+#include "shell.h"
 
 /**
- * _fork - program that creates process and execute
- * @p: array of pointer (args)
- * @l: input typed by the user
- * @a: count of pointers
- * @L: count of loops
- * @v: arguments in input
- * @e: env length
- * @m: copy of environmental variable
- * @f: complete input
- * Return: Nothing.
+ * get_environ - returns the string array copy of our environ
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
  */
-
-void _fork(char **p, char *l, int a, int L, char **v, int e, char **m, char *f)
+char **get_environ(info_t *info)
 {
-	pid_t child_pid;
-	int status;
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
 
-	child_pid = fork();
-	if (child_pid == -1)
+	return (info->environ);
+}
+
+/**
+ * _unsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: 1 on delete, 0 otherwise
+ * @var: the string env var property
+ */
+int _unsetenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
 	{
-		perror("Error");
-		exit(127);
-	}
-	if (child_pid == 0)
-	{
-		if (execve(p[0], p, m) == -1)
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
 		{
-			_put_err(p, L, 3, v);
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
 		}
-		free(f);
-		free(l);
-		gridfree(p, a);
-		gridfree(m, e);
-		/*printf("After execve\n");*/
-		exit(127);
+		node = node->next;
+		i++;
 	}
-	else
+	return (info->env_changed);
+}
+
+/**
+ * _setenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * @var: the string env var property
+ * @value: the string env var value
+ *  Return: Always 0
+ */
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
 	{
-		wait(&status);
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
 	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
 }
