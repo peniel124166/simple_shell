adb7b94779ebfba5f79fccc202e4eb337fa631f6
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2022-12-21 08:46:41 -0800

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -1,123 +1,115 @@
-#include "sshell.h"
+#include "shell.h"
 
 /**
- * _cd - finds if line input is cd builtin
- * @p: input of user, array of pointers
- * @loop: counter of loop
- * @v: arguments in input
- * @myenv: copy of environment variables
- * Return: -1 if not success 0 if exist cd in args[0]
+ * _myhistory - displays the history list, one command by line, preceded
+ *              with line numbers, starting at 0.
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
  */
-int _cd(char **p, int loop, char *v[], char **myenv)
+int _myhistory(info_t *info)
 {
-	char str[] = "cd";
-	int i = 0, cont = 0, valor = -1;
+	print_list(info->history);
+	return (0);
+}
 
-	while (p[0][i] != '\0')
+/**
+ * unset_alias - sets alias to string
+ * @info: parameter struct
+ * @str: the string alias
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int unset_alias(info_t *info, char *str)
+{
+	char *p, c;
+	int ret;
+
+	p = _strchr(str, '=');
+	if (!p)
+		return (1);
+	c = *p;
+	*p = 0;
+	ret = delete_node_at_index(&(info->alias),
+		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+	*p = c;
+	return (ret);
+}
+
+/**
+ * set_alias - sets alias to string
+ * @info: parameter struct
+ * @str: the string alias
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int set_alias(info_t *info, char *str)
+{
+	char *p;
+
+	p = _strchr(str, '=');
+	if (!p)
+		return (1);
+	if (!*++p)
+		return (unset_alias(info, str));
+
+	unset_alias(info, str);
+	return (add_node_end(&(info->alias), str, 0) == NULL);
+}
+
+/**
+ * print_alias - prints an alias string
+ * @node: the alias node
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int print_alias(list_t *node)
+{
+	char *p = NULL, *a = NULL;
+
+	if (node)
 	{
-		if (i < 2)
+		p = _strchr(node->str, '=');
+		for (a = node->str; a <= p; a++)
+			_putchar(*a);
+		_putchar('\'');
+		_puts(p + 1);
+		_puts("'\n");
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * _myalias - mimics the alias builtin (man alias)
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _myalias(info_t *info)
+{
+	int i = 0;
+	char *p = NULL;
+	list_t *node = NULL;
+
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
 		{
-			if (p[0][i] == str[i])
-				cont++;
+			print_alias(node);
+			node = node->next;
 		}
-		i++;
+		return (0);
 	}
-	if (i == 2)
-		cont++;
-
-	if (cont == 3)
+	for (i = 1; info->argv[i]; i++)
 	{
-		_iscd(p, loop, v, myenv);
-		valor = 0;
+		p = _strchr(info->argv[i], '=');
+		if (p)
+			set_alias(info, info->argv[i]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
 	}
-	else if (cont == 2)
-	{
-		_put_err(p, loop, 3, v);
-		valor = 0;
-	}
-	return (valor);
-}
 
-/**
- * _clean - clean buffer
- * @c: pointer to buf
- * Return: -1 if not success 0 if exist cd in args[0]
- */
-void _clean(char *c)
-{
-	int i;
-
-	for (i = 0; i < 2048; i++)
-		c[i] = 0;
-}
-
-/**
- * _fullcd - fill buffer
- * @f: pointer to buf
- * @aux: pointer to aux
- * Return: -1 if not success 0 if exist cd in args[0]
- */
-
-void _fullcd(char *f, char *aux)
-{
-	int w;
-
-	for (w = 0; aux[w] != '\0'; w++)
-		f[w] = aux[w];
-	for (; w < 2048; w++)
-		f[w] = 0;
-}
-/**
- * _iscd - Changes the current directory of the process.
- * @a: input of user, array of pointers
- * @loop: loops counter
- * @v: arguments in input
- * @myenv: copy of environment variables
- * Return:-1 if not find the directory or 0 if success
- */
-void _iscd(char **a, int loop, char *v[], char **myenv)
-{
-	int valor = 0;
-	static char buf[2048];
-	static int w = 1;
-	char aux[2048] = {0};
-
-	if (w == 1)
-	{
-		_updateoldpwd(getcwd(buf, 2048), myenv);
-		_fullcd(buf, _gethome(myenv));
-		w++;
-	}
-	if (a[1] == NULL)
-	{
-		_clean(buf);
-		getcwd(buf, 2048);
-
-		_updateoldpwd(buf, myenv);
-		valor = chdir((const char *)_gethome(myenv));
-		_updatepwd(_gethome(myenv), myenv);
-
-	}
-	else if (a[1][0] == '-' && a[1][1] == '\0')
-	{
-		_clean(aux);
-		getcwd(aux, 2048);
-
-		_updateoldpwd(aux, myenv);
-		write(STDOUT_FILENO, buf, 2048);
-		write(STDOUT_FILENO, "\n", 1);
-		valor = chdir((const char *)buf);
-		_updatepwd(buf, myenv);
-		_fullcd(buf, aux);
-	}
-	else
-	{
-		_clean(buf);
-		getcwd(buf, 2048);
-		_updateoldpwd(buf, myenv);
-		valor = chdir((const char *)a[1]);
-		_updatepwd(a[1], myenv);
-	}
-	if (valor == -1)
-		_put_err(a, loop, 1, v);
+	return (0);
 }
