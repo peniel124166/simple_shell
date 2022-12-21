3b49a80fe4f8c4afd402bf03524abe1a87b2916c
reem-3 <113852082+reem-3@users.noreply.github.com>
2022-12-21 12:00:42 -0800

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,54 +1,86 @@
-#include "sshell.h"
+#include "shell.h"
 
 /**
- * _realloc2 - change the size and copy the content
- * @a: string to add
- * @p: malloc pointer to reallocate
- * @old: old number of bytes
- * @new_size: new number of Bytes
- * Return: nothing
+ * is_cmd - determines if a file is an executable command
+ * @info: the info struct
+ * @path: path to the file
+ *
+ * Return: 1 if true, 0 otherwise
  */
-
-void *_realloc2(char *a, char *p, unsigned int old, unsigned int new_size)
+int is_cmd(info_t *info, char *path)
 {
-	char *pa = NULL;
-	unsigned int i, j = 0;
+	struct stat st;
 
-	if (new_size == old)
-		return (p);
-	if (p == NULL || a == NULL)
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
 	{
-		pa = _calloc(new_size + 1, sizeof(char));
-		if (!pa)
-			return (NULL);
-		return (pa);
+		return (1);
 	}
-	while (a[j] != '\0')
-		j++;
-	if (new_size == 0 && p != NULL)
-	{
-		free(p);
+	return (0);
+}
+
+/**
+ * dup_chars - duplicates characters
+ * @pathstr: the PATH string
+ * @start: starting index
+ * @stop: stopping index
+ *
+ * Return: pointer to new buffer
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
+
+/**
+ * find_path - finds this cmd in the PATH string
+ * @info: the info struct
+ * @pathstr: the PATH string
+ * @cmd: the cmd to find
+ *
+ * Return: full path of cmd if found or NULL
+ */
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int i = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
 		return (NULL);
-	}
-	if (new_size > old)
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
 	{
-		pa = _calloc(new_size + 1, sizeof(char));
-		if (!pa)
-			return (NULL);
-		for (i = 0; i < j; i++)
-			pa[i] = a[i];
-		for (; i <= new_size; i++)
-			pa[i] = *((char *)p + (i - j));
-		free(p);
+		if (is_cmd(info, cmd))
+			return (cmd);
 	}
-	else
+	while (1)
 	{
-		pa = _calloc(new_size, sizeof(char));
-		if (!pa)
-			return (NULL);
-		for (i = 0; i < new_size; i++)
-			pa[i] = *((char *)p + i);
-		free(p);
+		if (!pathstr[i] || pathstr[i] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, i);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[i])
+				break;
+			curr_pos = i;
+		}
+		i++;
 	}
-	return (pa);
+	return (NULL);
 }
